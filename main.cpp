52dc0554a0551eed768133fe6de6dd6c11830e95
Evgenii.Korchagov <evgenii.korchagov@kofax.com>
2022-04-11 10:58:34 +0300

    All done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <fstream>
 #include "nlohmann/json.hpp"
 
 int main()
 {
-    nlohmann::json dict;
-    std::cout << "Hello, World!" << std::endl;
+    nlohmann::json film;
+    film["origin"]="USA";
+    film["created"]=2003;
+    film["studio"]="Wiseau-Films";
+    film["writer"]="Tommy Wiseau";
+    film["title"]="The Room";
+    film["cast"]={"Tommy Wiseau as Johnny", "Juliette Danielle as Lisa","Greg Sestero as Mark"};
+
+    std::ofstream json_file("films.json");
+
+    json_file << film;
     return 0;
 }
